4878c0b7f445424ec40e3eef2fc8f1701f539173
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-06-24 14:00:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,90 +1,88 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
-#include "derivative.hpp"
+#include "random.hpp"
 
-using namespace std; using namespace d;
+using namespace std; using namespace rad;
 
 double activation_function(double S){
   return 1 / (1 + exp(-S));
 }
 
-int main(){
-
-
-  double n1, n2, v1, v2; // Variaveis [(len array 1), (len array 2), (input 1), (input 2)]
-
-  cin >> n1 >> n2;
-
-  double w;
-
-  vector<double> C1; // Array 1
-  vector<double> C2; // Array 2
-
-  vector<vector<double>> W = { // Vetor para guardar os pesos
-    {0.3, 0.4, 0.5, 0.6}, // 0[w0, w1, w2, w3]
-    {0.6, 0.7, 0.1, 0.1}, // 1[w4, w5, w6, w7]
-    {0.4, 0.7, 0.0, 0.1}, // 2[w8, w9, w10, w11] (Sigmoid [first])
-    {0.1, 0.6, 0.2, 0.2}, // 3[w12, w13, w14, w15]
-    {0.0, 0.0, 0.5, 0.2}, // 4[w16, w17, w18, w19]
-    {0.2, 0.5, 0.4, 0.3}, // 5[w20, w21, w22, w23]
-    {0.3, 0.4, 0.0, 0.9}, // 6[w24, w25, w26, w27]
-    {0.1, 0.6, 0.8, 0.0}, // 7[w28, w29, w30, w31]
+vector<double> W{
+    0.3, 0.4, 0.5, 0.6, 0.6, 0.7, 0.1,
+    0.1, 0.4, 0.7, 0.0, 0.1, 0.1, 0.6,
+    0.2, 0.2, 0.0, 0.0, 0.5, 0.2, 0.2,
+    0.5, 0.4, 0.3, 0.3, 0.4, 0.0, 0.9
   };
 
-  vector<vector<double>> C; 
+vector<double> C1; // Array 1
+vector<double> C2(4); // Array 2
 
-  C.resize(n1);
+int main(){
+  int N = 4;
+  double target = 0;
+  double n1 = 4, n2 = 4, v1, v2; // Variaveis [(len array 1), (len array 2), (input 1), (input 2)]
+
+  double w, Wi = 0;
 
   cin >> v1 >> v2; // inputs 1 e 2
 
-  for(int i = 0; i < n1; i++){
-    C1.push_back(W[0][i]*v1);
-    cout << v1 << " * " << W[0][i] << " = "<< C1[i] << endl;
-  }
-
-  cout << endl;
-
-  for(int i = 0; i < n1; i++){
-    cout << C1[i] << " + " << v2 << " * " << W[1][i] << " = ";
-    C1[i] += W[1][i]*v2;
-    cout << C1[i] << endl;
-  }
-
-  cout << endl;
-
-  for(int i = 0; i < n1; i++){
-    C1[i] = activation_function(C1[i]);
-    // cout << S << " + " << C1[i] << " * " << W[2][i] << " = ";
-    // S += C1[i]*W[2][i];
-    // cout << S << endl;
-  }
-
-  C2.resize(n2);
-
-  for(int i = 0; i < n2; i++){
-    for(int j = 0; j < n1; j++){
-      cout << "C2 " << i << ": " << C2[i] << " + " << C1[i] << " * " << W[i][j] << " = ";
-      C2[i] += C1[j]*W[2+i][j];
-      cout << C2[i] << endl;
+  while(N--){
+    Wi = 0;
+    for(int i = 0; i < 4; i++, Wi++){
+      C1.push_back(W[Wi]*v1);
+      cout << v1 << " * " << W[Wi] << " = "<< C1[i] << endl;
     }
+
+    cout << endl;
+
+    for(int i = 0; i < 4; i++, Wi++){
+      cout << C1[i] << " + " << v2 << " * " << W[Wi] << " = ";
+      C1[i] += W[Wi]*v2;
+      cout << C1[i] << endl;
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < 4; i++){
+      C1[i] = activation_function(C1[i]);
+      // cout << S << " + " << C1[i] << " * " << W[2][i] << " = ";
+      // S += C1[i]*W[2][i];
+      // cout << S << endl;
+    }
+
+    for(int i = 0; i < 4; i++){
+      for(int j = 0; j < n1; j++, Wi++){
+        cout << "C2 " << i << ": " << C2[i] << " + " << C1[j] << " * " << W[Wi] << " = ";
+        C2[i] += C1[j]*W[Wi];
+        cout << C2[i] << endl;
+      }
+    }
+
+    double S = 0;
+
+    for(int i = 0; i < n2; i++, Wi++){
+      C2[i] = activation_function(C2[i]);
+      cout << S << " + " << C2[i] << " * " << W[Wi] << " = ";
+      S += C2[i]*W[Wi];
+      cout << S << endl;
+    }
+
+    S = activation_function(S);
+    cout << endl;
+    printf("OUTPUT: %.4lf\n", S);
+    double ERROR = pow(target-S, 2);
+    printf("ERROR: %.4lf\n", ERROR);
+
+    double dE = S*(target-S);
+
+    double delta1 = dE*(1-activation_function(S));
+
+    W[27] = delta1*C2[3];
+    W[26] = delta1*C2[2];
+    W[25] = delta1*C2[1];
+    W[24] = delta1*C2[0];
+
   }
-
-  double S = 0;
-
-  for(int i = 0; i < n2; i++){
-    C2[i] = activation_function(C2[i]);
-    cout << S << " + " << C2[i] << " * " << W[2][i] << " = ";
-    S += C2[i]*W[6][i];
-    cout << S << endl;
-  }
-
-  S = activation_function(S);
-  printf("%.4lf\n", S);
-
-  return 0;
-
-  // S = output
-  // E = (S - target)²
-  // dE/dw16 = dE/dS
 }
