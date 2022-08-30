832b0b144f97f85c51236abd1ee0242019d54536
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-08-30 17:25:16 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,49 +9,42 @@
 
 using namespace Eigen;
 
+CsvReader csv("iris.csv");
+
+MatrixXd Input(4, 1);
+MatrixXd Target(3, 1);
+
+void setValue(const int i, MatrixXd &temp, string type){
+  for(int line = 0; line < temp.rows(); line++){
+    for(int column = 0; column < temp.cols(); column++){
+      if(type == "input")
+        temp(line, column) = csv.parse[i][line];
+      if(type == "target")
+        temp(line, column) = csv.parse[i][csv.parse[i].size() - Target.rows() + line];
+    }
+  }
+}
+
 int main(){
   ofstream myfile;
   myfile.open("output.txt");
   std::vector<unsigned int> N {4, 8, 16, 32, 32, 3};
-  MatrixXd Input(4, 1);
   MatrixXd Output;
-  MatrixXd Target(3, 1);
   double l_rate = 0.500;
   unsigned int epoch,Epochs = 150;
   double error;
 
-  CsvReader csv("iris.csv");
-
   Network net(N);
 
-  for(epoch=0;epoch<Epochs;epoch++){
-  //std::cout << "Epoch " << epoch+1 << std::endl;
+  for(epoch = 0; epoch < Epochs; epoch++){
   error = 0;
     for(int i = 0; i < 150; i++){
-      MatrixXd Input(4, 1);
-      MatrixXd Target(3, 1);
 
-      for(int line = 0; line < Input.rows(); line++){
-        for(int column = 0; column < Input.cols(); column++){
-          Input(line, column) = csv.parse[i][line];
-        }
-      }
-
-      for(int line = 0; line < Target.rows(); line++){
-        for(int column = 0; column < Target.cols(); column++){
-          Target(line, column) = csv.parse[i][csv.parse[i].size() - 3 + line ];
-        }
-      }
-
-      // std::cout << Input << std::endl;
-      // std::cout << Target << std::endl;
+      setValue(i, Input, "input");
+      setValue(i, Target, "target");
       
       Output = net.feedFoward(Input);
-      //std::cout << Output << std::endl;
-      //std::cout << "Output: " << Output << std::endl;
-      //std::cout << "Error: ";
       error += net.showError(Output, Target)(0, 0);
-      //myfile << error(0, 0) << '\n';
       net.backPropagation(Output, Target, l_rate);
     }
     error /= 150.0;
